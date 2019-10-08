48c2be39cf31eb23d7698b7cd46647cf3c439053
stpatrck <rizzin@gmail.com>
2019-10-08 19:53:08 -0400

    Split up launch view model concerns
    Separated the single launch view model into two distinct view models to
    better enforce separation of concerns.

---
+++ app/src/main/java/com/stpatrck/playground/koinexample/viewmodels/LaunchDetailViewModel.kt
@@ -0,0 +1,26 @@
+package com.stpatrck.playground.koinexample.viewmodels
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.stpatrck.playground.koinexample.exceptions.RecordNotFoundException
+import com.stpatrck.playground.koinexample.repos.LaunchRepository
+
+class LaunchDetailViewModel(private val launchRepo: LaunchRepository) : ViewModel() {
+
+    fun getLaunchDetails(launchName: String): String {
+
+        return try {
+            launchRepo.getLaunch(launchName)
+        } catch (e: RecordNotFoundException) {
+            e.message!!
+        } catch (e: Exception) {
+            Log.e(
+                "LaunchListViewModel",
+                "Exception encountered while fetching launch details with name = [$launchName].",
+                e
+            )
+            "Unknown error occurred. Try again."
+        }
+    }
+
+}
