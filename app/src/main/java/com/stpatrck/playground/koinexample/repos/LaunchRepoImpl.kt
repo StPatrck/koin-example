57bcbbeb65022a0194b5701947a9bb3261166fab
stpatrck <rizzin@gmail.com>
2019-10-08 15:13:58 -0400

    Add `LaunchRepo` implementation
    Created a basic `LaunchRepo` implementation that returns a static list of launch names.

---
+++ app/src/main/java/com/stpatrck/playground/koinexample/repos/LaunchRepoImpl.kt
@@ -0,0 +1,15 @@
+package com.stpatrck.playground.koinexample.repos
+
+import com.stpatrck.playground.koinexample.exceptions.RecordNotFoundException
+
+class LaunchRepoImpl : LaunchRepository {
+    override fun getLaunches(): List<String> {
+        return listOf("Alpha", "Beta", "Charlie")
+    }
+
+    @Throws(RecordNotFoundException::class)
+    override fun getLaunch(identifier: String): String {
+        return getLaunches().find { it.equals(identifier, ignoreCase = true) }
+            ?: throw RecordNotFoundException("No launch found with id = [$identifier]")
+    }
+}
